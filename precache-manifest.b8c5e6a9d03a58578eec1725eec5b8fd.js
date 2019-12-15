842eadb369ef86e307ad7005b6a50c46503b9614
Niels Lerche Sørensen <nieslerches@gmail.com>
2019-12-15 21:10:50 +0100

    Updates

---
+++ precache-manifest.b8c5e6a9d03a58578eec1725eec5b8fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42ec47edd468eb708bb5c2978e1d314",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "5e0c47fa7586ab29f0ef",
+    "url": "/kanban/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2819598fc59485b6bce4",
+    "url": "/kanban/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "5e0c47fa7586ab29f0ef",
+    "url": "/kanban/static/js/2.d50939ea.chunk.js"
+  },
+  {
+    "revision": "2819598fc59485b6bce4",
+    "url": "/kanban/static/js/main.1a8f9aa7.chunk.js"
+  },
+  {
+    "revision": "f7d8c7ca3fa90e5901df",
+    "url": "/kanban/static/js/runtime-main.eb2d9d6b.js"
+  }
+]);
